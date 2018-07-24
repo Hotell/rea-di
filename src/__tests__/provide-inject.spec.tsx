d6167c6f9caccac7d9b28c68f0d2bd7b4fe4810a
Martin Hochel <hochelmartin@gmail.com>
2018-07-24 22:56:17 +0200

    test: add tests for Provider/Inject hierarchical injectors

--- src/__tests__/provide-inject.spec.tsx
+++ src/__tests__/provide-inject.spec.tsx
@@ -1,4 +1,6 @@
 // tslint:disable:no-magic-numbers
+// tslint:disable:jsx-no-lambda
+// tslint:disable:no-shadowed-variable
 import React, { Component } from 'react'
 
 import { mount } from 'enzyme'
@@ -7,6 +9,7 @@ import { Inject } from '../inject'
 import { Provider } from '../provider'
 import { Counter } from './setup/components'
 import { CounterService, Logger } from './setup/services'
+import { select } from './setup/utils'
 
 class CounterModule extends Component<{ title: string }> {
   render() {
@@ -81,5 +84,98 @@ describe(`Provide/Inject`, () => {
     expect(counterLogger.log).toHaveBeenCalledTimes(3)
 
     tree.unmount()
+  })
+
+  it(`should properly create hierarchical injectors`, () => {
+    class LoggerMock implements Logger {
+      log = jest.fn((...args: any[]) => void 0)
+    }
+    // tslint:disable:prefer-const
+    let parentLoggerInstance!: LoggerMock
+    let childLoggerInstance!: LoggerMock
+
+    const App = () => {
+      return (
+        <Provider provide={[{ provide: Logger, useClass: LoggerMock }]}>
+          <div data-test="parentInjector">
+            <Inject providers={{ logger: Logger }}>
+              {(fromParent) => {
+                parentLoggerInstance = fromParent.logger as LoggerMock
+
+                return (
+                  <div>
+                    <button
+                      onClick={() => {
+                        fromParent.logger.log('from parent')
+                      }}
+                    >
+                      log from parent
+                    </button>
+                    <Provider
+                      provide={[{ provide: Logger, useClass: LoggerMock }]}
+                    >
+                      <div data-test="childInjector">
+                        <Inject providers={{ logger: Logger }}>
+                          {(fromChild) => {
+                            childLoggerInstance = fromChild.logger as LoggerMock
+
+                            return (
+                              <div>
+                                <button
+                                  onClick={() => {
+                                    fromParent.logger.log('parent from child')
+                                  }}
+                                >
+                                  log parent from child
+                                </button>
+                                <button
+                                  onClick={() => {
+                                    fromChild.logger.log('from child')
+                                  }}
+                                >
+                                  log from child
+                                </button>
+                              </div>
+                            )
+                          }}
+                        </Inject>
+                      </div>
+                    </Provider>
+                  </div>
+                )
+              }}
+            </Inject>
+          </div>
+        </Provider>
+      )
+    }
+
+    const tree = mount(<App />)
+
+    const parentBtn = tree.find(`${select('parentInjector')} button`).at(0)
+    const childrenButtons = tree.find(`${select('childInjector')} button`)
+    const childrenBtnThatLogsParent = childrenButtons.at(0)
+    const childrenBtnThatLogsChild = childrenButtons.at(1)
+
+    expect(parentLoggerInstance.log).not.toHaveBeenCalled()
+
+    parentBtn.simulate('click')
+
+    expect(parentLoggerInstance.log).toHaveBeenCalledWith('from parent')
+
+    childrenBtnThatLogsParent.simulate('click')
+
+    expect(parentLoggerInstance.log).toHaveBeenLastCalledWith(
+      'parent from child'
+    )
+    expect(parentLoggerInstance.log).toHaveBeenCalledTimes(2)
+
+    expect(childLoggerInstance.log).not.toHaveBeenCalled()
+
+    childrenBtnThatLogsChild.simulate('click')
+
+    expect(parentLoggerInstance.log).toHaveBeenCalledTimes(2)
+    expect(childLoggerInstance.log).toHaveBeenCalledTimes(1)
+    expect(childLoggerInstance.log).toHaveBeenLastCalledWith('from child')
   })
 })
